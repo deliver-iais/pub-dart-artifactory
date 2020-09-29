dc349223334436074db862fce8ef2d34e47252e4
GitLab Runner <gitlab-runner@localhost.localdomain>
2020-09-29 14:10:07 +0330

    UPDATE

---
+++ lib/pub/v1/models/media.pbjson.dart
@@ -0,0 +1,22 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/media.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const Media$json = const {
+  '1': 'Media',
+  '2': const [
+    const {'1': 'created_on', '3': 1, '4': 1, '5': 3, '10': 'createdOn'},
+    const {'1': 'room_uid', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'roomUid'},
+    const {'1': 'created_by', '3': 3, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'createdBy'},
+    const {'1': 'message_id', '3': 4, '4': 1, '5': 3, '10': 'messageId'},
+    const {'1': 'file', '3': 5, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'file'},
+    const {'1': 'link', '3': 6, '4': 1, '5': 9, '9': 0, '10': 'link'},
+  ],
+  '8': const [
+    const {'1': 'type'},
+  ],
+};
+
