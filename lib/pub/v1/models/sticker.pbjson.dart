a695085b540626475eaf4ddbc6fbbd1da2c2ce5e
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-01-04 12:07:00 +0330

    UPDATE

---
+++ lib/pub/v1/models/sticker.pbjson.dart
@@ -0,0 +1,17 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/sticker.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const Sticker$json = const {
+  '1': 'Sticker',
+  '2': const [
+    const {'1': 'uuid', '3': 1, '4': 1, '5': 9, '10': 'uuid'},
+    const {'1': 'id', '3': 2, '4': 1, '5': 9, '10': 'id'},
+    const {'1': 'width', '3': 5, '4': 1, '5': 5, '10': 'width'},
+    const {'1': 'height', '3': 6, '4': 1, '5': 5, '10': 'height'},
+  ],
+};
+
