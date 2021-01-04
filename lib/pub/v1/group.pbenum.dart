3b7f21427f52948685c4b7b56afd5d0e583c6657
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-01-04 14:00:17 +0330

    UPDATE

---
+++ lib/pub/v1/group.pbenum.dart
@@ -0,0 +1,24 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/group.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+// ignore_for_file: UNDEFINED_SHOWN_NAME,UNUSED_SHOWN_NAME
+import 'dart:core' as $core;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class GetAccessTokenReq_AccessField extends $pb.ProtobufEnum {
+  static const GetAccessTokenReq_AccessField CHANGE_AVATAR = GetAccessTokenReq_AccessField._(0, 'CHANGE_AVATAR');
+
+  static const $core.List<GetAccessTokenReq_AccessField> values = <GetAccessTokenReq_AccessField> [
+    CHANGE_AVATAR,
+  ];
+
+  static final $core.Map<$core.int, GetAccessTokenReq_AccessField> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static GetAccessTokenReq_AccessField valueOf($core.int value) => _byValue[value];
+
+  const GetAccessTokenReq_AccessField._($core.int v, $core.String n) : super(v, n);
+}
+
