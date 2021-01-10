5eb958ca05792dd24258c824df9f718e3233e7b2
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-01-10 14:41:22 +0330

    UPDATE

---
+++ lib/pub/v1/models/avatar.pbjson.dart
@@ -0,0 +1,18 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/avatar.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const Avatar$json = const {
+  '1': 'Avatar',
+  '2': const [
+    const {'1': 'category', '3': 1, '4': 1, '5': 14, '6': '.proto.pub.v1.models.Categories', '10': 'category'},
+    const {'1': 'node', '3': 2, '4': 1, '5': 9, '10': 'node'},
+    const {'1': 'created_on', '3': 3, '4': 1, '5': 3, '10': 'createdOn'},
+    const {'1': 'file_uuid', '3': 4, '4': 1, '5': 9, '10': 'fileUuid'},
+    const {'1': 'file_name', '3': 5, '4': 1, '5': 9, '10': 'fileName'},
+  ],
+};
+
