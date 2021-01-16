dc4dabdb3bad0916be5e8533751a76a4e2f8cd29
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-01-16 14:12:29 +0330

    UPDATE

---
+++ lib/pub/v1/core.pbenum.dart
@@ -0,0 +1,61 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/core.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+// ignore_for_file: UNDEFINED_SHOWN_NAME,UNUSED_SHOWN_NAME
+import 'dart:core' as $core;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class Pong_StreamInputCapabilityStatus extends $pb.ProtobufEnum {
+  static const Pong_StreamInputCapabilityStatus HIGH_AVAILABILITY = Pong_StreamInputCapabilityStatus._(0, 'HIGH_AVAILABILITY');
+  static const Pong_StreamInputCapabilityStatus NOT_AVAILABLE = Pong_StreamInputCapabilityStatus._(1, 'NOT_AVAILABLE');
+  static const Pong_StreamInputCapabilityStatus LOW_AVAILABILITY = Pong_StreamInputCapabilityStatus._(2, 'LOW_AVAILABILITY');
+
+  static const $core.List<Pong_StreamInputCapabilityStatus> values = <Pong_StreamInputCapabilityStatus> [
+    HIGH_AVAILABILITY,
+    NOT_AVAILABLE,
+    LOW_AVAILABILITY,
+  ];
+
+  static final $core.Map<$core.int, Pong_StreamInputCapabilityStatus> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static Pong_StreamInputCapabilityStatus valueOf($core.int value) => _byValue[value];
+
+  const Pong_StreamInputCapabilityStatus._($core.int v, $core.String n) : super(v, n);
+}
+
+class ErrorOnPacket_Error extends $pb.ProtobufEnum {
+  static const ErrorOnPacket_Error UNKNOWN = ErrorOnPacket_Error._(0, 'UNKNOWN');
+  static const ErrorOnPacket_Error BAD_REQUEST = ErrorOnPacket_Error._(1, 'BAD_REQUEST');
+  static const ErrorOnPacket_Error UNAUTHENTICATED = ErrorOnPacket_Error._(2, 'UNAUTHENTICATED');
+  static const ErrorOnPacket_Error PERMISSION_DENIED = ErrorOnPacket_Error._(3, 'PERMISSION_DENIED');
+  static const ErrorOnPacket_Error NOT_FOUND = ErrorOnPacket_Error._(4, 'NOT_FOUND');
+  static const ErrorOnPacket_Error CONFLICT = ErrorOnPacket_Error._(5, 'CONFLICT');
+  static const ErrorOnPacket_Error RESOURCE_EXHAUSTED = ErrorOnPacket_Error._(6, 'RESOURCE_EXHAUSTED');
+  static const ErrorOnPacket_Error INTERNAL = ErrorOnPacket_Error._(7, 'INTERNAL');
+  static const ErrorOnPacket_Error UNIMPLEMENTED = ErrorOnPacket_Error._(8, 'UNIMPLEMENTED');
+  static const ErrorOnPacket_Error UNAVAILABLE = ErrorOnPacket_Error._(9, 'UNAVAILABLE');
+  static const ErrorOnPacket_Error DEADLINE_EXCEEDED = ErrorOnPacket_Error._(10, 'DEADLINE_EXCEEDED');
+
+  static const $core.List<ErrorOnPacket_Error> values = <ErrorOnPacket_Error> [
+    UNKNOWN,
+    BAD_REQUEST,
+    UNAUTHENTICATED,
+    PERMISSION_DENIED,
+    NOT_FOUND,
+    CONFLICT,
+    RESOURCE_EXHAUSTED,
+    INTERNAL,
+    UNIMPLEMENTED,
+    UNAVAILABLE,
+    DEADLINE_EXCEEDED,
+  ];
+
+  static final $core.Map<$core.int, ErrorOnPacket_Error> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static ErrorOnPacket_Error valueOf($core.int value) => _byValue[value];
+
+  const ErrorOnPacket_Error._($core.int v, $core.String n) : super(v, n);
+}
+
---
+++ lib/pub/v1/models/message.pbjson.dart
@@ -0,0 +1,200 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/message.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const Text$json = const {
+  '1': 'Text',
+  '2': const [
+    const {'1': 'text', '3': 1, '4': 1, '5': 9, '10': 'text'},
+  ],
+};
+
+const Location$json = const {
+  '1': 'Location',
+  '2': const [
+    const {'1': 'latitude', '3': 1, '4': 1, '5': 1, '10': 'latitude'},
+    const {'1': 'longitude', '3': 2, '4': 1, '5': 1, '10': 'longitude'},
+  ],
+};
+
+const PaymentTransaction$json = const {
+  '1': 'PaymentTransaction',
+  '2': const [
+    const {'1': 'uuid', '3': 1, '4': 1, '5': 9, '10': 'uuid'},
+  ],
+};
+
+const Poll$json = const {
+  '1': 'Poll',
+  '2': const [
+    const {'1': 'uuid', '3': 1, '4': 1, '5': 9, '10': 'uuid'},
+    const {'1': 'title', '3': 2, '4': 1, '5': 9, '10': 'title'},
+    const {'1': 'number_of_options', '3': 3, '4': 1, '5': 5, '10': 'numberOfOptions'},
+  ],
+};
+
+const Form$json = const {
+  '1': 'Form',
+  '2': const [
+    const {'1': 'uuid', '3': 1, '4': 1, '5': 9, '10': 'uuid'},
+    const {'1': 'title', '3': 2, '4': 1, '5': 9, '10': 'title'},
+  ],
+};
+
+const LiveLocation$json = const {
+  '1': 'LiveLocation',
+  '2': const [
+    const {'1': 'uuid', '3': 1, '4': 1, '5': 9, '10': 'uuid'},
+  ],
+};
+
+const PersistentEvent$json = const {
+  '1': 'PersistentEvent',
+  '2': const [
+    const {'1': 'muc_specific_persistent_event', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.MucSpecificPersistentEvent', '9': 0, '10': 'mucSpecificPersistentEvent'},
+    const {'1': 'message_manipulation_persistent_event', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.MessageManipulationPersistentEvent', '9': 0, '10': 'messageManipulationPersistentEvent'},
+    const {'1': 'admin_specific_persistent_event', '3': 3, '4': 1, '5': 11, '6': '.proto.pub.v1.models.AdminSpecificPersistentEvent', '9': 0, '10': 'adminSpecificPersistentEvent'},
+  ],
+  '8': const [
+    const {'1': 'type'},
+  ],
+};
+
+const MucSpecificPersistentEvent$json = const {
+  '1': 'MucSpecificPersistentEvent',
+  '2': const [
+    const {'1': 'issuer', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'issuer'},
+    const {'1': 'issue', '3': 2, '4': 1, '5': 14, '6': '.proto.pub.v1.models.MucSpecificPersistentEvent.Issue', '10': 'issue'},
+    const {'1': 'assignee', '3': 3, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'assignee'},
+  ],
+  '4': const [MucSpecificPersistentEvent_Issue$json],
+};
+
+const MucSpecificPersistentEvent_Issue$json = const {
+  '1': 'Issue',
+  '2': const [
+    const {'1': 'KICK_USER', '2': 0},
+    const {'1': 'ADD_USER', '2': 1},
+    const {'1': 'PIN_MESSAGE', '2': 2},
+    const {'1': 'NAME_CHANGED', '2': 3},
+    const {'1': 'AVATAR_CHANGED', '2': 4},
+    const {'1': 'MUC_CREATED', '2': 5},
+    const {'1': 'LEAVE_USER', '2': 6},
+    const {'1': 'JOINED_USER', '2': 7},
+  ],
+};
+
+const MessageManipulationPersistentEvent$json = const {
+  '1': 'MessageManipulationPersistentEvent',
+  '2': const [
+    const {'1': 'action', '3': 1, '4': 1, '5': 14, '6': '.proto.pub.v1.models.MessageManipulationPersistentEvent.Action', '10': 'action'},
+    const {'1': 'message_id', '3': 2, '4': 1, '5': 3, '10': 'messageId'},
+    const {'1': 'manipulation_time', '3': 3, '4': 1, '5': 3, '10': 'manipulationTime'},
+  ],
+  '4': const [MessageManipulationPersistentEvent_Action$json],
+};
+
+const MessageManipulationPersistentEvent_Action$json = const {
+  '1': 'Action',
+  '2': const [
+    const {'1': 'EDITED', '2': 0},
+    const {'1': 'DELETED', '2': 1},
+  ],
+};
+
+const AdminSpecificPersistentEvent$json = const {
+  '1': 'AdminSpecificPersistentEvent',
+  '2': const [
+    const {'1': 'event', '3': 1, '4': 1, '5': 14, '6': '.proto.pub.v1.models.AdminSpecificPersistentEvent.Event', '10': 'event'},
+  ],
+  '4': const [AdminSpecificPersistentEvent_Event$json],
+};
+
+const AdminSpecificPersistentEvent_Event$json = const {
+  '1': 'Event',
+  '2': const [
+    const {'1': 'NEW_CONTACT_ADDED', '2': 0},
+  ],
+};
+
+const MessageDeliveryAck$json = const {
+  '1': 'MessageDeliveryAck',
+  '2': const [
+    const {'1': 'packet_id', '3': 1, '4': 1, '5': 9, '10': 'packetId'},
+    const {'1': 'id', '3': 2, '4': 1, '5': 3, '10': 'id'},
+    const {'1': 'time', '3': 3, '4': 1, '5': 3, '10': 'time'},
+    const {'1': 'from', '3': 4, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'from'},
+    const {'1': 'to', '3': 5, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'to'},
+  ],
+};
+
+const Message$json = const {
+  '1': 'Message',
+  '2': const [
+    const {'1': 'packet_id', '3': 1, '4': 1, '5': 9, '10': 'packetId'},
+    const {'1': 'id', '3': 2, '4': 1, '5': 3, '10': 'id'},
+    const {'1': 'time', '3': 4, '4': 1, '5': 3, '10': 'time'},
+    const {'1': 'from', '3': 5, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'from'},
+    const {'1': 'to', '3': 6, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'to'},
+    const {'1': 'reply_to_id', '3': 7, '4': 1, '5': 3, '10': 'replyToId'},
+    const {'1': 'forward_from', '3': 8, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'forwardFrom'},
+    const {'1': 'edited', '3': 9, '4': 1, '5': 8, '10': 'edited'},
+    const {'1': 'encrypted', '3': 10, '4': 1, '5': 8, '10': 'encrypted'},
+    const {'1': 'text', '3': 11, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Text', '9': 0, '10': 'text'},
+    const {'1': 'file', '3': 12, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'file'},
+    const {'1': 'sticker', '3': 13, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'sticker'},
+    const {'1': 'location', '3': 14, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Location', '9': 0, '10': 'location'},
+    const {'1': 'live_location', '3': 15, '4': 1, '5': 11, '6': '.proto.pub.v1.models.LiveLocation', '9': 0, '10': 'liveLocation'},
+    const {'1': 'poll', '3': 16, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Poll', '9': 0, '10': 'poll'},
+    const {'1': 'form', '3': 18, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Form', '9': 0, '10': 'form'},
+    const {'1': 'persist_event', '3': 19, '4': 1, '5': 11, '6': '.proto.pub.v1.models.PersistentEvent', '9': 0, '10': 'persistEvent'},
+  ],
+  '8': const [
+    const {'1': 'type'},
+  ],
+};
+
+const MessageByClient$json = const {
+  '1': 'MessageByClient',
+  '2': const [
+    const {'1': 'packet_id', '3': 1, '4': 1, '5': 9, '10': 'packetId'},
+    const {'1': 'to', '3': 6, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '8': const {}, '10': 'to'},
+    const {'1': 'reply_to_id', '3': 7, '4': 1, '5': 3, '10': 'replyToId'},
+    const {'1': 'forward_from', '3': 8, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'forwardFrom'},
+    const {'1': 'encrypted', '3': 10, '4': 1, '5': 8, '10': 'encrypted'},
+    const {'1': 'text', '3': 11, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Text', '9': 0, '10': 'text'},
+    const {'1': 'file', '3': 12, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'file'},
+    const {'1': 'sticker', '3': 13, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'sticker'},
+    const {'1': 'location', '3': 14, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Location', '9': 0, '10': 'location'},
+    const {'1': 'live_location', '3': 15, '4': 1, '5': 11, '6': '.proto.pub.v1.models.LiveLocation', '9': 0, '10': 'liveLocation'},
+    const {'1': 'poll', '3': 16, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Poll', '9': 0, '10': 'poll'},
+  ],
+  '8': const [
+    const {'1': 'type', '2': const {}},
+  ],
+};
+
+const MessageByBot$json = const {
+  '1': 'MessageByBot',
+  '2': const [
+    const {'1': 'packet_id', '3': 1, '4': 1, '5': 9, '10': 'packetId'},
+    const {'1': 'to', '3': 6, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '8': const {}, '10': 'to'},
+    const {'1': 'reply_to_id', '3': 7, '4': 1, '5': 3, '10': 'replyToId'},
+    const {'1': 'forward_from', '3': 8, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'forwardFrom'},
+    const {'1': 'encrypted', '3': 10, '4': 1, '5': 8, '10': 'encrypted'},
+    const {'1': 'text', '3': 11, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Text', '9': 0, '10': 'text'},
+    const {'1': 'file', '3': 12, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'file'},
+    const {'1': 'sticker', '3': 13, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'sticker'},
+    const {'1': 'location', '3': 14, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Location', '9': 0, '10': 'location'},
+    const {'1': 'live_location', '3': 15, '4': 1, '5': 11, '6': '.proto.pub.v1.models.LiveLocation', '9': 0, '10': 'liveLocation'},
+    const {'1': 'poll', '3': 16, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Poll', '9': 0, '10': 'poll'},
+    const {'1': 'form', '3': 18, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Form', '9': 0, '10': 'form'},
+  ],
+  '8': const [
+    const {'1': 'type', '2': const {}},
+  ],
+};
+
