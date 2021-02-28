95369d6530631f503e93c248280b31931477d80c
root <root@localhost.localdomain>
2021-02-28 11:14:02 +0330

    UPDATE to 1.7.0-rc.5

---
+++ lib/pub/v1/avatar.pb.dart
@@ -0,0 +1,192 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/avatar.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+import 'models/avatar.pb.dart' as $12;
+import 'models/uid.pb.dart' as $13;
+
+class AddAvatarReq extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('AddAvatarReq', package: const $pb.PackageName('proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..aOM<$12.Avatar>(1, 'avatar', subBuilder: $12.Avatar.create)
+    ..aOS(2, 'token')
+    ..hasRequiredFields = false
+  ;
+
+  AddAvatarReq._() : super();
+  factory AddAvatarReq() => create();
+  factory AddAvatarReq.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory AddAvatarReq.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  AddAvatarReq clone() => AddAvatarReq()..mergeFromMessage(this);
+  AddAvatarReq copyWith(void Function(AddAvatarReq) updates) => super.copyWith((message) => updates(message as AddAvatarReq));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static AddAvatarReq create() => AddAvatarReq._();
+  AddAvatarReq createEmptyInstance() => create();
+  static $pb.PbList<AddAvatarReq> createRepeated() => $pb.PbList<AddAvatarReq>();
+  @$core.pragma('dart2js:noInline')
+  static AddAvatarReq getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<AddAvatarReq>(create);
+  static AddAvatarReq _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $12.Avatar get avatar => $_getN(0);
+  @$pb.TagNumber(1)
+  set avatar($12.Avatar v) { setField(1, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasAvatar() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearAvatar() => clearField(1);
+  @$pb.TagNumber(1)
+  $12.Avatar ensureAvatar() => $_ensure(0);
+
+  @$pb.TagNumber(2)
+  $core.String get token => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set token($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasToken() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearToken() => clearField(2);
+}
+
+class AddAvatarRes extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('AddAvatarRes', package: const $pb.PackageName('proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..hasRequiredFields = false
+  ;
+
+  AddAvatarRes._() : super();
+  factory AddAvatarRes() => create();
+  factory AddAvatarRes.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory AddAvatarRes.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  AddAvatarRes clone() => AddAvatarRes()..mergeFromMessage(this);
+  AddAvatarRes copyWith(void Function(AddAvatarRes) updates) => super.copyWith((message) => updates(message as AddAvatarRes));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static AddAvatarRes create() => AddAvatarRes._();
+  AddAvatarRes createEmptyInstance() => create();
+  static $pb.PbList<AddAvatarRes> createRepeated() => $pb.PbList<AddAvatarRes>();
+  @$core.pragma('dart2js:noInline')
+  static AddAvatarRes getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<AddAvatarRes>(create);
+  static AddAvatarRes _defaultInstance;
+}
+
+class RemoveAvatarReq extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('RemoveAvatarReq', package: const $pb.PackageName('proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..aOM<$12.Avatar>(1, 'avatar', subBuilder: $12.Avatar.create)
+    ..aOS(2, 'token')
+    ..hasRequiredFields = false
+  ;
+
+  RemoveAvatarReq._() : super();
+  factory RemoveAvatarReq() => create();
+  factory RemoveAvatarReq.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory RemoveAvatarReq.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  RemoveAvatarReq clone() => RemoveAvatarReq()..mergeFromMessage(this);
+  RemoveAvatarReq copyWith(void Function(RemoveAvatarReq) updates) => super.copyWith((message) => updates(message as RemoveAvatarReq));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static RemoveAvatarReq create() => RemoveAvatarReq._();
+  RemoveAvatarReq createEmptyInstance() => create();
+  static $pb.PbList<RemoveAvatarReq> createRepeated() => $pb.PbList<RemoveAvatarReq>();
+  @$core.pragma('dart2js:noInline')
+  static RemoveAvatarReq getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<RemoveAvatarReq>(create);
+  static RemoveAvatarReq _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $12.Avatar get avatar => $_getN(0);
+  @$pb.TagNumber(1)
+  set avatar($12.Avatar v) { setField(1, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasAvatar() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearAvatar() => clearField(1);
+  @$pb.TagNumber(1)
+  $12.Avatar ensureAvatar() => $_ensure(0);
+
+  @$pb.TagNumber(2)
+  $core.String get token => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set token($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasToken() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearToken() => clearField(2);
+}
+
+class RemoveAvatarRes extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('RemoveAvatarRes', package: const $pb.PackageName('proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..hasRequiredFields = false
+  ;
+
+  RemoveAvatarRes._() : super();
+  factory RemoveAvatarRes() => create();
+  factory RemoveAvatarRes.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory RemoveAvatarRes.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  RemoveAvatarRes clone() => RemoveAvatarRes()..mergeFromMessage(this);
+  RemoveAvatarRes copyWith(void Function(RemoveAvatarRes) updates) => super.copyWith((message) => updates(message as RemoveAvatarRes));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static RemoveAvatarRes create() => RemoveAvatarRes._();
+  RemoveAvatarRes createEmptyInstance() => create();
+  static $pb.PbList<RemoveAvatarRes> createRepeated() => $pb.PbList<RemoveAvatarRes>();
+  @$core.pragma('dart2js:noInline')
+  static RemoveAvatarRes getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<RemoveAvatarRes>(create);
+  static RemoveAvatarRes _defaultInstance;
+}
+
+class GetAvatarReq extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('GetAvatarReq', package: const $pb.PackageName('proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..pc<$13.Uid>(1, 'uidList', $pb.PbFieldType.PM, subBuilder: $13.Uid.create)
+    ..hasRequiredFields = false
+  ;
+
+  GetAvatarReq._() : super();
+  factory GetAvatarReq() => create();
+  factory GetAvatarReq.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory GetAvatarReq.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  GetAvatarReq clone() => GetAvatarReq()..mergeFromMessage(this);
+  GetAvatarReq copyWith(void Function(GetAvatarReq) updates) => super.copyWith((message) => updates(message as GetAvatarReq));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarReq create() => GetAvatarReq._();
+  GetAvatarReq createEmptyInstance() => create();
+  static $pb.PbList<GetAvatarReq> createRepeated() => $pb.PbList<GetAvatarReq>();
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarReq getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<GetAvatarReq>(create);
+  static GetAvatarReq _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.List<$13.Uid> get uidList => $_getList(0);
+}
+
+class GetAvatarRes extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('GetAvatarRes', package: const $pb.PackageName('proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..pc<$12.Avatar>(1, 'avatar', $pb.PbFieldType.PM, subBuilder: $12.Avatar.create)
+    ..hasRequiredFields = false
+  ;
+
+  GetAvatarRes._() : super();
+  factory GetAvatarRes() => create();
+  factory GetAvatarRes.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory GetAvatarRes.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  GetAvatarRes clone() => GetAvatarRes()..mergeFromMessage(this);
+  GetAvatarRes copyWith(void Function(GetAvatarRes) updates) => super.copyWith((message) => updates(message as GetAvatarRes));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarRes create() => GetAvatarRes._();
+  GetAvatarRes createEmptyInstance() => create();
+  static $pb.PbList<GetAvatarRes> createRepeated() => $pb.PbList<GetAvatarRes>();
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarRes getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<GetAvatarRes>(create);
+  static GetAvatarRes _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.List<$12.Avatar> get avatar => $_getList(0);
+}
+
---
+++ lib/pub/v1/models/avatar.pb.dart
@@ -0,0 +1,85 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/avatar.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+import 'dart:core' as $core;
+
+import 'package:fixnum/fixnum.dart' as $fixnum;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+import 'categories.pbenum.dart' as $11;
+
+class Avatar extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('Avatar', package: const $pb.PackageName('proto.pub.v1.models'), createEmptyInstance: create)
+    ..e<$11.Categories>(1, 'category', $pb.PbFieldType.OE, defaultOrMaker: $11.Categories.USER, valueOf: $11.Categories.valueOf, enumValues: $11.Categories.values)
+    ..aOS(2, 'node')
+    ..aInt64(3, 'createdOn')
+    ..aOS(4, 'fileUuid')
+    ..aOS(5, 'fileName')
+    ..hasRequiredFields = false
+  ;
+
+  Avatar._() : super();
+  factory Avatar() => create();
+  factory Avatar.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory Avatar.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  Avatar clone() => Avatar()..mergeFromMessage(this);
+  Avatar copyWith(void Function(Avatar) updates) => super.copyWith((message) => updates(message as Avatar));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static Avatar create() => Avatar._();
+  Avatar createEmptyInstance() => create();
+  static $pb.PbList<Avatar> createRepeated() => $pb.PbList<Avatar>();
+  @$core.pragma('dart2js:noInline')
+  static Avatar getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Avatar>(create);
+  static Avatar _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $11.Categories get category => $_getN(0);
+  @$pb.TagNumber(1)
+  set category($11.Categories v) { setField(1, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasCategory() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearCategory() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get node => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set node($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasNode() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearNode() => clearField(2);
+
+  @$pb.TagNumber(3)
+  $fixnum.Int64 get createdOn => $_getI64(2);
+  @$pb.TagNumber(3)
+  set createdOn($fixnum.Int64 v) { $_setInt64(2, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasCreatedOn() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearCreatedOn() => clearField(3);
+
+  @$pb.TagNumber(4)
+  $core.String get fileUuid => $_getSZ(3);
+  @$pb.TagNumber(4)
+  set fileUuid($core.String v) { $_setString(3, v); }
+  @$pb.TagNumber(4)
+  $core.bool hasFileUuid() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearFileUuid() => clearField(4);
+
+  @$pb.TagNumber(5)
+  $core.String get fileName => $_getSZ(4);
+  @$pb.TagNumber(5)
+  set fileName($core.String v) { $_setString(4, v); }
+  @$pb.TagNumber(5)
+  $core.bool hasFileName() => $_has(4);
+  @$pb.TagNumber(5)
+  void clearFileName() => clearField(5);
+}
+
