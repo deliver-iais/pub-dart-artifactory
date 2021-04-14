a37630ae97fb3b541ff2eceac5e2eedaf435ed8e
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-04-14 17:54:57 +0430

    UPDATE to 1.7.0-rc.48

---
+++ lib/pub/v1/models/muc.pbenum.dart
@@ -0,0 +1,43 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/muc.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+// ignore_for_file: UNDEFINED_SHOWN_NAME,UNUSED_SHOWN_NAME
+import 'dart:core' as $core;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class Role extends $pb.ProtobufEnum {
+  static const Role NONE = Role._(0, 'NONE');
+  static const Role MEMBER = Role._(1, 'MEMBER');
+  static const Role ADMIN = Role._(2, 'ADMIN');
+  static const Role OWNER = Role._(3, 'OWNER');
+
+  static const $core.List<Role> values = <Role> [
+    NONE,
+    MEMBER,
+    ADMIN,
+    OWNER,
+  ];
+
+  static final $core.Map<$core.int, Role> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static Role valueOf($core.int value) => _byValue[value];
+
+  const Role._($core.int v, $core.String n) : super(v, n);
+}
+
+class AccessField extends $pb.ProtobufEnum {
+  static const AccessField CHANGE_AVATAR = AccessField._(0, 'CHANGE_AVATAR');
+
+  static const $core.List<AccessField> values = <AccessField> [
+    CHANGE_AVATAR,
+  ];
+
+  static final $core.Map<$core.int, AccessField> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static AccessField valueOf($core.int value) => _byValue[value];
+
+  const AccessField._($core.int v, $core.String n) : super(v, n);
+}
+
