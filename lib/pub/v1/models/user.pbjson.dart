6b44852c56360b489f5bed154a09f6e6893fabef
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-04-21 14:00:47 +0430

    UPDATE to 1.7.0-rc.63

---
+++ lib/pub/v1/models/user.pbjson.dart
@@ -0,0 +1,32 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/user.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const UserAsContact$json = const {
+  '1': 'UserAsContact',
+  '2': const [
+    const {'1': 'uid', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'uid'},
+    const {'1': 'phone_number', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.PhoneNumber', '10': 'phoneNumber'},
+    const {'1': 'first_name', '3': 3, '4': 1, '5': 9, '10': 'firstName'},
+    const {'1': 'last_name', '3': 4, '4': 1, '5': 9, '10': 'lastName'},
+    const {'1': 'description', '3': 5, '4': 1, '5': 9, '10': 'description'},
+    const {'1': 'email', '3': 6, '4': 1, '5': 9, '10': 'email'},
+  ],
+};
+
+const UserPrivateData$json = const {
+  '1': 'UserPrivateData',
+  '2': const [
+    const {'1': 'uid', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'uid'},
+    const {'1': 'phone_number', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.PhoneNumber', '10': 'phoneNumber'},
+    const {'1': 'password_hash', '3': 3, '4': 1, '5': 9, '10': 'passwordHash'},
+    const {'1': 'first_name', '3': 4, '4': 1, '5': 9, '10': 'firstName'},
+    const {'1': 'last_name', '3': 5, '4': 1, '5': 9, '10': 'lastName'},
+    const {'1': 'description', '3': 6, '4': 1, '5': 9, '10': 'description'},
+    const {'1': 'email', '3': 7, '4': 1, '5': 9, '10': 'email'},
+  ],
+};
+
