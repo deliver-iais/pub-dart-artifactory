f8bf78410040c8c7cb61f70afa88de77f2c8951b
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-05-25 15:14:42 +0430

    UPDATE to 1.7.0-rc.69

---
+++ lib/pub/v1/models/share_private_data.pbjson.dart
@@ -0,0 +1,29 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/share_private_data.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const PrivateDataType$json = const {
+  '1': 'PrivateDataType',
+  '2': const [
+    const {'1': 'PHONE_NUMBER', '2': 0},
+    const {'1': 'USERNAME', '2': 1},
+  ],
+};
+
+const SharePrivateDataRequest$json = const {
+  '1': 'SharePrivateDataRequest',
+  '2': const [
+    const {'1': 'data', '3': 1, '4': 1, '5': 14, '6': '.proto.pub.v1.models.PrivateDataType', '10': 'data'},
+  ],
+};
+
+const SharePrivateDataAcceptance$json = const {
+  '1': 'SharePrivateDataAcceptance',
+  '2': const [
+    const {'1': 'data', '3': 1, '4': 1, '5': 14, '6': '.proto.pub.v1.models.PrivateDataType', '10': 'data'},
+  ],
+};
+
