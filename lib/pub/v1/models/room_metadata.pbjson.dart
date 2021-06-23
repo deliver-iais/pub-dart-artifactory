e85ed4c5c26b22379efd2de53bf523cff1bfefec
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-06-23 09:29:18 +0430

    UPDATE to 1.7.0-rc.76

---
+++ lib/pub/v1/models/room_metadata.pbjson.dart
@@ -0,0 +1,17 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/room_metadata.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const RoomMetadata$json = const {
+  '1': 'RoomMetadata',
+  '2': const [
+    const {'1': 'room_uid', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'roomUid'},
+    const {'1': 'last_message_id', '3': 2, '4': 1, '5': 3, '10': 'lastMessageId'},
+    const {'1': 'last_message_time', '3': 3, '4': 1, '5': 3, '10': 'lastMessageTime'},
+    const {'1': 'last_current_user_sent_message_id', '3': 4, '4': 1, '5': 3, '10': 'lastCurrentUserSentMessageId'},
+  ],
+};
+
