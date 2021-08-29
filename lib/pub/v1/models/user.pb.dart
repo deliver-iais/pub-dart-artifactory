91a86e48e5f1c849653e90ed48183aa60dbfce31
Seyed Mohammad Ali BaniFatemi <smabanifatemi78@gmail.com>
2021-08-29 17:09:49 +0430

    UPDATE to 1.8.6-beta.11

---
+++ lib/pub/v1/models/user.pb.dart
@@ -0,0 +1,270 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/user.proto
+//
+// @dart = 2.7
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+import 'uid.pb.dart' as $15;
+import 'phone.pb.dart' as $28;
+
+class UserAsContact extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'UserAsContact', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.models'), createEmptyInstance: create)
+    ..aOM<$15.Uid>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'uid', subBuilder: $15.Uid.create)
+    ..aOM<$28.PhoneNumber>(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'phoneNumber', subBuilder: $28.PhoneNumber.create)
+    ..aOS(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'firstName')
+    ..aOS(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastName')
+    ..aOS(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'description')
+    ..aOS(6, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'email')
+    ..hasRequiredFields = false
+  ;
+
+  UserAsContact._() : super();
+  factory UserAsContact({
+    $15.Uid uid,
+    $28.PhoneNumber phoneNumber,
+    $core.String firstName,
+    $core.String lastName,
+    $core.String description,
+    $core.String email,
+  }) {
+    final _result = create();
+    if (uid != null) {
+      _result.uid = uid;
+    }
+    if (phoneNumber != null) {
+      _result.phoneNumber = phoneNumber;
+    }
+    if (firstName != null) {
+      _result.firstName = firstName;
+    }
+    if (lastName != null) {
+      _result.lastName = lastName;
+    }
+    if (description != null) {
+      _result.description = description;
+    }
+    if (email != null) {
+      _result.email = email;
+    }
+    return _result;
+  }
+  factory UserAsContact.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory UserAsContact.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  UserAsContact clone() => UserAsContact()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  UserAsContact copyWith(void Function(UserAsContact) updates) => super.copyWith((message) => updates(message as UserAsContact)) as UserAsContact; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static UserAsContact create() => UserAsContact._();
+  UserAsContact createEmptyInstance() => create();
+  static $pb.PbList<UserAsContact> createRepeated() => $pb.PbList<UserAsContact>();
+  @$core.pragma('dart2js:noInline')
+  static UserAsContact getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<UserAsContact>(create);
+  static UserAsContact _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $15.Uid get uid => $_getN(0);
+  @$pb.TagNumber(1)
+  set uid($15.Uid v) { setField(1, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasUid() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearUid() => clearField(1);
+  @$pb.TagNumber(1)
+  $15.Uid ensureUid() => $_ensure(0);
+
+  @$pb.TagNumber(2)
+  $28.PhoneNumber get phoneNumber => $_getN(1);
+  @$pb.TagNumber(2)
+  set phoneNumber($28.PhoneNumber v) { setField(2, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasPhoneNumber() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearPhoneNumber() => clearField(2);
+  @$pb.TagNumber(2)
+  $28.PhoneNumber ensurePhoneNumber() => $_ensure(1);
+
+  @$pb.TagNumber(3)
+  $core.String get firstName => $_getSZ(2);
+  @$pb.TagNumber(3)
+  set firstName($core.String v) { $_setString(2, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasFirstName() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearFirstName() => clearField(3);
+
+  @$pb.TagNumber(4)
+  $core.String get lastName => $_getSZ(3);
+  @$pb.TagNumber(4)
+  set lastName($core.String v) { $_setString(3, v); }
+  @$pb.TagNumber(4)
+  $core.bool hasLastName() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearLastName() => clearField(4);
+
+  @$pb.TagNumber(5)
+  $core.String get description => $_getSZ(4);
+  @$pb.TagNumber(5)
+  set description($core.String v) { $_setString(4, v); }
+  @$pb.TagNumber(5)
+  $core.bool hasDescription() => $_has(4);
+  @$pb.TagNumber(5)
+  void clearDescription() => clearField(5);
+
+  @$pb.TagNumber(6)
+  $core.String get email => $_getSZ(5);
+  @$pb.TagNumber(6)
+  set email($core.String v) { $_setString(5, v); }
+  @$pb.TagNumber(6)
+  $core.bool hasEmail() => $_has(5);
+  @$pb.TagNumber(6)
+  void clearEmail() => clearField(6);
+}
+
+class UserPrivateData extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'UserPrivateData', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.models'), createEmptyInstance: create)
+    ..aOM<$15.Uid>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'uid', subBuilder: $15.Uid.create)
+    ..aOM<$28.PhoneNumber>(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'phoneNumber', subBuilder: $28.PhoneNumber.create)
+    ..aOS(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'passwordHash')
+    ..aOS(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'firstName')
+    ..aOS(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastName')
+    ..aOS(6, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'description')
+    ..aOS(7, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'email')
+    ..hasRequiredFields = false
+  ;
+
+  UserPrivateData._() : super();
+  factory UserPrivateData({
+    $15.Uid uid,
+    $28.PhoneNumber phoneNumber,
+    $core.String passwordHash,
+    $core.String firstName,
+    $core.String lastName,
+    $core.String description,
+    $core.String email,
+  }) {
+    final _result = create();
+    if (uid != null) {
+      _result.uid = uid;
+    }
+    if (phoneNumber != null) {
+      _result.phoneNumber = phoneNumber;
+    }
+    if (passwordHash != null) {
+      _result.passwordHash = passwordHash;
+    }
+    if (firstName != null) {
+      _result.firstName = firstName;
+    }
+    if (lastName != null) {
+      _result.lastName = lastName;
+    }
+    if (description != null) {
+      _result.description = description;
+    }
+    if (email != null) {
+      _result.email = email;
+    }
+    return _result;
+  }
+  factory UserPrivateData.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory UserPrivateData.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  UserPrivateData clone() => UserPrivateData()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  UserPrivateData copyWith(void Function(UserPrivateData) updates) => super.copyWith((message) => updates(message as UserPrivateData)) as UserPrivateData; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static UserPrivateData create() => UserPrivateData._();
+  UserPrivateData createEmptyInstance() => create();
+  static $pb.PbList<UserPrivateData> createRepeated() => $pb.PbList<UserPrivateData>();
+  @$core.pragma('dart2js:noInline')
+  static UserPrivateData getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<UserPrivateData>(create);
+  static UserPrivateData _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $15.Uid get uid => $_getN(0);
+  @$pb.TagNumber(1)
+  set uid($15.Uid v) { setField(1, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasUid() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearUid() => clearField(1);
+  @$pb.TagNumber(1)
+  $15.Uid ensureUid() => $_ensure(0);
+
+  @$pb.TagNumber(2)
+  $28.PhoneNumber get phoneNumber => $_getN(1);
+  @$pb.TagNumber(2)
+  set phoneNumber($28.PhoneNumber v) { setField(2, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasPhoneNumber() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearPhoneNumber() => clearField(2);
+  @$pb.TagNumber(2)
+  $28.PhoneNumber ensurePhoneNumber() => $_ensure(1);
+
+  @$pb.TagNumber(3)
+  $core.String get passwordHash => $_getSZ(2);
+  @$pb.TagNumber(3)
+  set passwordHash($core.String v) { $_setString(2, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasPasswordHash() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearPasswordHash() => clearField(3);
+
+  @$pb.TagNumber(4)
+  $core.String get firstName => $_getSZ(3);
+  @$pb.TagNumber(4)
+  set firstName($core.String v) { $_setString(3, v); }
+  @$pb.TagNumber(4)
+  $core.bool hasFirstName() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearFirstName() => clearField(4);
+
+  @$pb.TagNumber(5)
+  $core.String get lastName => $_getSZ(4);
+  @$pb.TagNumber(5)
+  set lastName($core.String v) { $_setString(4, v); }
+  @$pb.TagNumber(5)
+  $core.bool hasLastName() => $_has(4);
+  @$pb.TagNumber(5)
+  void clearLastName() => clearField(5);
+
+  @$pb.TagNumber(6)
+  $core.String get description => $_getSZ(5);
+  @$pb.TagNumber(6)
+  set description($core.String v) { $_setString(5, v); }
+  @$pb.TagNumber(6)
+  $core.bool hasDescription() => $_has(5);
+  @$pb.TagNumber(6)
+  void clearDescription() => clearField(6);
+
+  @$pb.TagNumber(7)
+  $core.String get email => $_getSZ(6);
+  @$pb.TagNumber(7)
+  set email($core.String v) { $_setString(6, v); }
+  @$pb.TagNumber(7)
+  $core.bool hasEmail() => $_has(6);
+  @$pb.TagNumber(7)
+  void clearEmail() => clearField(7);
+}
+
