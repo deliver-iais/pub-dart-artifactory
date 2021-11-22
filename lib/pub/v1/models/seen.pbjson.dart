5dc44bc96b7c5736a9c650add6095b1286245241
GitLab Runner <gitlab-runner@localhost.localdomain>
2021-11-22 15:36:23 +0330

    UPDATE to 1.10.0

---
+++ lib/pub/v1/models/seen.pbjson.dart
@@ -0,0 +1,45 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/seen.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use seenDescriptor instead')
+const Seen$json = const {
+  '1': 'Seen',
+  '2': const [
+    const {'1': 'from', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'from'},
+    const {'1': 'to', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'to'},
+    const {'1': 'id', '3': 3, '4': 1, '5': 3, '10': 'id'},
+  ],
+};
+
+/// Descriptor for `Seen`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List seenDescriptor = $convert.base64Decode('CgRTZWVuEiwKBGZyb20YASABKAsyGC5wcm90by5wdWIudjEubW9kZWxzLlVpZFIEZnJvbRIoCgJ0bxgCIAEoCzIYLnByb3RvLnB1Yi52MS5tb2RlbHMuVWlkUgJ0bxIOCgJpZBgDIAEoA1ICaWQ=');
+@$core.Deprecated('Use seenByClientDescriptor instead')
+const SeenByClient$json = const {
+  '1': 'SeenByClient',
+  '2': const [
+    const {'1': 'to', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'to'},
+    const {'1': 'id', '3': 3, '4': 1, '5': 3, '10': 'id'},
+  ],
+};
+
+/// Descriptor for `SeenByClient`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List seenByClientDescriptor = $convert.base64Decode('CgxTZWVuQnlDbGllbnQSKAoCdG8YAiABKAsyGC5wcm90by5wdWIudjEubW9kZWxzLlVpZFICdG8SDgoCaWQYAyABKANSAmlk');
+@$core.Deprecated('Use seenByBotDescriptor instead')
+const SeenByBot$json = const {
+  '1': 'SeenByBot',
+  '2': const [
+    const {'1': 'from', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'from'},
+    const {'1': 'to', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'to'},
+    const {'1': 'id', '3': 3, '4': 1, '5': 3, '10': 'id'},
+  ],
+};
+
+/// Descriptor for `SeenByBot`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List seenByBotDescriptor = $convert.base64Decode('CglTZWVuQnlCb3QSLAoEZnJvbRgBIAEoCzIYLnByb3RvLnB1Yi52MS5tb2RlbHMuVWlkUgRmcm9tEigKAnRvGAIgASgLMhgucHJvdG8ucHViLnYxLm1vZGVscy5VaWRSAnRvEg4KAmlkGAMgASgDUgJpZA==');
---
+++ lib/pub/v1/profile.pbenum.dart
@@ -0,0 +1,41 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/profile.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+// ignore_for_file: UNDEFINED_SHOWN_NAME
+import 'dart:core' as $core;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class VerificationType extends $pb.ProtobufEnum {
+  static const VerificationType MESSAGE = VerificationType._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'MESSAGE');
+  static const VerificationType SMS = VerificationType._(1, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'SMS');
+
+  static const $core.List<VerificationType> values = <VerificationType> [
+    MESSAGE,
+    SMS,
+  ];
+
+  static final $core.Map<$core.int, VerificationType> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static VerificationType? valueOf($core.int value) => _byValue[value];
+
+  const VerificationType._($core.int v, $core.String n) : super(v, n);
+}
+
+class AccessTokenRes_Status extends $pb.ProtobufEnum {
+  static const AccessTokenRes_Status OK = AccessTokenRes_Status._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'OK');
+  static const AccessTokenRes_Status PASSWORD_PROTECTED = AccessTokenRes_Status._(1, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'PASSWORD_PROTECTED');
+
+  static const $core.List<AccessTokenRes_Status> values = <AccessTokenRes_Status> [
+    OK,
+    PASSWORD_PROTECTED,
+  ];
+
+  static final $core.Map<$core.int, AccessTokenRes_Status> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static AccessTokenRes_Status? valueOf($core.int value) => _byValue[value];
+
+  const AccessTokenRes_Status._($core.int v, $core.String n) : super(v, n);
+}
+
