a0592abee624a811de551011dd7ef8e403b5b80e
GitLab Runner <gitlab-runner@localhost.localdomain>
2021-12-14 19:04:06 +0330

    UPDATE to 1.11.4

---
+++ lib/pub/v1/models/user.pbjson.dart
@@ -0,0 +1,41 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/user.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use userAsContactDescriptor instead')
+const UserAsContact$json = const {
+  '1': 'UserAsContact',
+  '2': const [
+    const {'1': 'uid', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'uid'},
+    const {'1': 'phone_number', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.PhoneNumber', '10': 'phoneNumber'},
+    const {'1': 'first_name', '3': 3, '4': 1, '5': 9, '10': 'firstName'},
+    const {'1': 'last_name', '3': 4, '4': 1, '5': 9, '10': 'lastName'},
+    const {'1': 'description', '3': 5, '4': 1, '5': 9, '10': 'description'},
+    const {'1': 'email', '3': 6, '4': 1, '5': 9, '10': 'email'},
+  ],
+};
+
+/// Descriptor for `UserAsContact`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List userAsContactDescriptor = $convert.base64Decode('Cg1Vc2VyQXNDb250YWN0EioKA3VpZBgBIAEoCzIYLnByb3RvLnB1Yi52MS5tb2RlbHMuVWlkUgN1aWQSQwoMcGhvbmVfbnVtYmVyGAIgASgLMiAucHJvdG8ucHViLnYxLm1vZGVscy5QaG9uZU51bWJlclILcGhvbmVOdW1iZXISHQoKZmlyc3RfbmFtZRgDIAEoCVIJZmlyc3ROYW1lEhsKCWxhc3RfbmFtZRgEIAEoCVIIbGFzdE5hbWUSIAoLZGVzY3JpcHRpb24YBSABKAlSC2Rlc2NyaXB0aW9uEhQKBWVtYWlsGAYgASgJUgVlbWFpbA==');
+@$core.Deprecated('Use userPrivateDataDescriptor instead')
+const UserPrivateData$json = const {
+  '1': 'UserPrivateData',
+  '2': const [
+    const {'1': 'uid', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'uid'},
+    const {'1': 'phone_number', '3': 2, '4': 1, '5': 11, '6': '.proto.pub.v1.models.PhoneNumber', '10': 'phoneNumber'},
+    const {'1': 'password_hash', '3': 3, '4': 1, '5': 9, '10': 'passwordHash'},
+    const {'1': 'first_name', '3': 4, '4': 1, '5': 9, '10': 'firstName'},
+    const {'1': 'last_name', '3': 5, '4': 1, '5': 9, '10': 'lastName'},
+    const {'1': 'description', '3': 6, '4': 1, '5': 9, '10': 'description'},
+    const {'1': 'email', '3': 7, '4': 1, '5': 9, '10': 'email'},
+  ],
+};
+
+/// Descriptor for `UserPrivateData`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List userPrivateDataDescriptor = $convert.base64Decode('Cg9Vc2VyUHJpdmF0ZURhdGESKgoDdWlkGAEgASgLMhgucHJvdG8ucHViLnYxLm1vZGVscy5VaWRSA3VpZBJDCgxwaG9uZV9udW1iZXIYAiABKAsyIC5wcm90by5wdWIudjEubW9kZWxzLlBob25lTnVtYmVyUgtwaG9uZU51bWJlchIjCg1wYXNzd29yZF9oYXNoGAMgASgJUgxwYXNzd29yZEhhc2gSHQoKZmlyc3RfbmFtZRgEIAEoCVIJZmlyc3ROYW1lEhsKCWxhc3RfbmFtZRgFIAEoCVIIbGFzdE5hbWUSIAoLZGVzY3JpcHRpb24YBiABKAlSC2Rlc2NyaXB0aW9uEhQKBWVtYWlsGAcgASgJUgVlbWFpbA==');
