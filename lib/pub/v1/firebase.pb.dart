b80780c12c9322036881069c05c7c2e514e764cb
GitLab Runner <gitlab-runner@localhost.localdomain>
2021-12-14 18:07:58 +0330

    UPDATE to 1.11.3

---
+++ lib/pub/v1/firebase.pb.dart
@@ -0,0 +1,87 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/firebase.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class RegistrationReq extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'RegistrationReq', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.firebase'), createEmptyInstance: create)
+    ..aOS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'tokenId')
+    ..hasRequiredFields = false
+  ;
+
+  RegistrationReq._() : super();
+  factory RegistrationReq({
+    $core.String? tokenId,
+  }) {
+    final _result = create();
+    if (tokenId != null) {
+      _result.tokenId = tokenId;
+    }
+    return _result;
+  }
+  factory RegistrationReq.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory RegistrationReq.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  RegistrationReq clone() => RegistrationReq()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  RegistrationReq copyWith(void Function(RegistrationReq) updates) => super.copyWith((message) => updates(message as RegistrationReq)) as RegistrationReq; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static RegistrationReq create() => RegistrationReq._();
+  RegistrationReq createEmptyInstance() => create();
+  static $pb.PbList<RegistrationReq> createRepeated() => $pb.PbList<RegistrationReq>();
+  @$core.pragma('dart2js:noInline')
+  static RegistrationReq getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<RegistrationReq>(create);
+  static RegistrationReq? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get tokenId => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set tokenId($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasTokenId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearTokenId() => clearField(1);
+}
+
+class RegistrationRes extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'RegistrationRes', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.firebase'), createEmptyInstance: create)
+    ..hasRequiredFields = false
+  ;
+
+  RegistrationRes._() : super();
+  factory RegistrationRes() => create();
+  factory RegistrationRes.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory RegistrationRes.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  RegistrationRes clone() => RegistrationRes()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  RegistrationRes copyWith(void Function(RegistrationRes) updates) => super.copyWith((message) => updates(message as RegistrationRes)) as RegistrationRes; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static RegistrationRes create() => RegistrationRes._();
+  RegistrationRes createEmptyInstance() => create();
+  static $pb.PbList<RegistrationRes> createRepeated() => $pb.PbList<RegistrationRes>();
+  @$core.pragma('dart2js:noInline')
+  static RegistrationRes getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<RegistrationRes>(create);
+  static RegistrationRes? _defaultInstance;
+}
+
