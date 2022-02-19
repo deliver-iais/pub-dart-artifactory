b6140b282993f47b013819ef5920b3cf067759a4
GitLab Runner <gitlab-runner@localhost.localdomain>
2022-02-19 19:03:01 +0330

    UPDATE to 1.11.17

---
+++ lib/pub/v1/models/categories.pbjson.dart
@@ -0,0 +1,25 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/categories.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use categoriesDescriptor instead')
+const Categories$json = const {
+  '1': 'Categories',
+  '2': const [
+    const {'1': 'USER', '2': 0},
+    const {'1': 'SYSTEM', '2': 1},
+    const {'1': 'GROUP', '2': 2},
+    const {'1': 'CHANNEL', '2': 3},
+    const {'1': 'BOT', '2': 4},
+    const {'1': 'STORE', '2': 5},
+  ],
+};
+
+/// Descriptor for `Categories`. Decode as a `google.protobuf.EnumDescriptorProto`.
+final $typed_data.Uint8List categoriesDescriptor = $convert.base64Decode('CgpDYXRlZ29yaWVzEggKBFVTRVIQABIKCgZTWVNURU0QARIJCgVHUk9VUBACEgsKB0NIQU5ORUwQAxIHCgNCT1QQBBIJCgVTVE9SRRAF');
