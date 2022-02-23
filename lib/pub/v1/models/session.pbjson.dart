76fa76881ebc86b9d5357831db7a48e90ea2a567
GitLab Runner <gitlab-runner@localhost.localdomain>
2022-02-23 12:25:49 +0330

    UPDATE to 1.11.19

---
+++ lib/pub/v1/models/session.pbjson.dart
@@ -0,0 +1,25 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/session.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use sessionDescriptor instead')
+const Session$json = const {
+  '1': 'Session',
+  '2': const [
+    const {'1': 'session_id', '3': 1, '4': 1, '5': 9, '10': 'sessionId'},
+    const {'1': 'node', '3': 2, '4': 1, '5': 9, '10': 'node'},
+    const {'1': 'device', '3': 3, '4': 1, '5': 9, '10': 'device'},
+    const {'1': 'ip', '3': 4, '4': 1, '5': 9, '10': 'ip'},
+    const {'1': 'created_on', '3': 5, '4': 1, '5': 3, '10': 'createdOn'},
+    const {'1': 'platform', '3': 6, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Platform', '10': 'platform'},
+  ],
+};
+
+/// Descriptor for `Session`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List sessionDescriptor = $convert.base64Decode('CgdTZXNzaW9uEh0KCnNlc3Npb25faWQYASABKAlSCXNlc3Npb25JZBISCgRub2RlGAIgASgJUgRub2RlEhYKBmRldmljZRgDIAEoCVIGZGV2aWNlEg4KAmlwGAQgASgJUgJpcBIdCgpjcmVhdGVkX29uGAUgASgDUgljcmVhdGVkT24SOQoIcGxhdGZvcm0YBiABKAsyHS5wcm90by5wdWIudjEubW9kZWxzLlBsYXRmb3JtUghwbGF0Zm9ybQ==');
---
+++ lib/pub/v1/sticker.pbgrpc.dart
@@ -0,0 +1,114 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/sticker.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+import 'dart:async' as $async;
+
+import 'dart:core' as $core;
+
+import 'package:grpc/service_api.dart' as $grpc;
+import 'sticker.pb.dart' as $12;
+export 'sticker.pb.dart';
+
+class StickerServiceClient extends $grpc.Client {
+  static final _$getTrendPacks =
+      $grpc.ClientMethod<$12.GetTrendPacksReq, $12.GetTrendPacksRes>(
+          '/proto.pub.v1.sticker.StickerService/GetTrendPacks',
+          ($12.GetTrendPacksReq value) => value.writeToBuffer(),
+          ($core.List<$core.int> value) =>
+              $12.GetTrendPacksRes.fromBuffer(value));
+  static final _$getStickerPackByUUID = $grpc.ClientMethod<
+          $12.GetStickerPackByUUIDReq, $12.GetStickerPackByUUIDRes>(
+      '/proto.pub.v1.sticker.StickerService/GetStickerPackByUUID',
+      ($12.GetStickerPackByUUIDReq value) => value.writeToBuffer(),
+      ($core.List<$core.int> value) =>
+          $12.GetStickerPackByUUIDRes.fromBuffer(value));
+  static final _$getStickerPackByID =
+      $grpc.ClientMethod<$12.GetStickerPackByIDReq, $12.GetStickerPackByIDRes>(
+          '/proto.pub.v1.sticker.StickerService/GetStickerPackByID',
+          ($12.GetStickerPackByIDReq value) => value.writeToBuffer(),
+          ($core.List<$core.int> value) =>
+              $12.GetStickerPackByIDRes.fromBuffer(value));
+
+  StickerServiceClient($grpc.ClientChannel channel,
+      {$grpc.CallOptions? options,
+      $core.Iterable<$grpc.ClientInterceptor>? interceptors})
+      : super(channel, options: options, interceptors: interceptors);
+
+  $grpc.ResponseFuture<$12.GetTrendPacksRes> getTrendPacks(
+      $12.GetTrendPacksReq request,
+      {$grpc.CallOptions? options}) {
+    return $createUnaryCall(_$getTrendPacks, request, options: options);
+  }
+
+  $grpc.ResponseFuture<$12.GetStickerPackByUUIDRes> getStickerPackByUUID(
+      $12.GetStickerPackByUUIDReq request,
+      {$grpc.CallOptions? options}) {
+    return $createUnaryCall(_$getStickerPackByUUID, request, options: options);
+  }
+
+  $grpc.ResponseFuture<$12.GetStickerPackByIDRes> getStickerPackByID(
+      $12.GetStickerPackByIDReq request,
+      {$grpc.CallOptions? options}) {
+    return $createUnaryCall(_$getStickerPackByID, request, options: options);
+  }
+}
+
+abstract class StickerServiceBase extends $grpc.Service {
+  $core.String get $name => 'proto.pub.v1.sticker.StickerService';
+
+  StickerServiceBase() {
+    $addMethod($grpc.ServiceMethod<$12.GetTrendPacksReq, $12.GetTrendPacksRes>(
+        'GetTrendPacks',
+        getTrendPacks_Pre,
+        false,
+        false,
+        ($core.List<$core.int> value) => $12.GetTrendPacksReq.fromBuffer(value),
+        ($12.GetTrendPacksRes value) => value.writeToBuffer()));
+    $addMethod($grpc.ServiceMethod<$12.GetStickerPackByUUIDReq,
+            $12.GetStickerPackByUUIDRes>(
+        'GetStickerPackByUUID',
+        getStickerPackByUUID_Pre,
+        false,
+        false,
+        ($core.List<$core.int> value) =>
+            $12.GetStickerPackByUUIDReq.fromBuffer(value),
+        ($12.GetStickerPackByUUIDRes value) => value.writeToBuffer()));
+    $addMethod($grpc.ServiceMethod<$12.GetStickerPackByIDReq,
+            $12.GetStickerPackByIDRes>(
+        'GetStickerPackByID',
+        getStickerPackByID_Pre,
+        false,
+        false,
+        ($core.List<$core.int> value) =>
+            $12.GetStickerPackByIDReq.fromBuffer(value),
+        ($12.GetStickerPackByIDRes value) => value.writeToBuffer()));
+  }
+
+  $async.Future<$12.GetTrendPacksRes> getTrendPacks_Pre($grpc.ServiceCall call,
+      $async.Future<$12.GetTrendPacksReq> request) async {
+    return getTrendPacks(call, await request);
+  }
+
+  $async.Future<$12.GetStickerPackByUUIDRes> getStickerPackByUUID_Pre(
+      $grpc.ServiceCall call,
+      $async.Future<$12.GetStickerPackByUUIDReq> request) async {
+    return getStickerPackByUUID(call, await request);
+  }
+
+  $async.Future<$12.GetStickerPackByIDRes> getStickerPackByID_Pre(
+      $grpc.ServiceCall call,
+      $async.Future<$12.GetStickerPackByIDReq> request) async {
+    return getStickerPackByID(call, await request);
+  }
+
+  $async.Future<$12.GetTrendPacksRes> getTrendPacks(
+      $grpc.ServiceCall call, $12.GetTrendPacksReq request);
+  $async.Future<$12.GetStickerPackByUUIDRes> getStickerPackByUUID(
+      $grpc.ServiceCall call, $12.GetStickerPackByUUIDReq request);
+  $async.Future<$12.GetStickerPackByIDRes> getStickerPackByID(
+      $grpc.ServiceCall call, $12.GetStickerPackByIDReq request);
+}
