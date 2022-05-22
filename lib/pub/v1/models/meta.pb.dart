c8da0df6c6fcdd5bdd93d34d3bf447c0287c4e2f
deliver-protocol-publisher <bitbeter@mmmm.cc>
2022-05-22 13:54:23 +0000

    UPDATE to 1.12.13-rc.6

---
+++ lib/pub/v1/models/meta.pb.dart
@@ -0,0 +1,227 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/meta.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+import 'dart:core' as $core;
+
+import 'package:fixnum/fixnum.dart' as $fixnum;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+import 'uid.pb.dart' as $17;
+import 'file.pb.dart' as $15;
+import 'call.pb.dart' as $26;
+
+enum Meta_Type {
+  file, 
+  link, 
+  call, 
+  notSet
+}
+
+class Meta extends $pb.GeneratedMessage {
+  static const $core.Map<$core.int, Meta_Type> _Meta_TypeByTag = {
+    6 : Meta_Type.file,
+    7 : Meta_Type.link,
+    8 : Meta_Type.call,
+    0 : Meta_Type.notSet
+  };
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Meta', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.models'), createEmptyInstance: create)
+    ..oo(0, [6, 7, 8])
+    ..aInt64(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'createdOn')
+    ..aInt64(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'index')
+    ..aOM<$17.Uid>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'sender', subBuilder: $17.Uid.create)
+    ..aInt64(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'messageId')
+    ..aOB(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'encrypted')
+    ..aOM<$15.File>(6, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'file', subBuilder: $15.File.create)
+    ..aOM<Link>(7, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'link', subBuilder: Link.create)
+    ..aOM<$26.CallInfo>(8, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'call', subBuilder: $26.CallInfo.create)
+    ..hasRequiredFields = false
+  ;
+
+  Meta._() : super();
+  factory Meta({
+    $fixnum.Int64? createdOn,
+    $fixnum.Int64? index,
+    $17.Uid? sender,
+    $fixnum.Int64? messageId,
+    $core.bool? encrypted,
+    $15.File? file,
+    Link? link,
+    $26.CallInfo? call,
+  }) {
+    final _result = create();
+    if (createdOn != null) {
+      _result.createdOn = createdOn;
+    }
+    if (index != null) {
+      _result.index = index;
+    }
+    if (sender != null) {
+      _result.sender = sender;
+    }
+    if (messageId != null) {
+      _result.messageId = messageId;
+    }
+    if (encrypted != null) {
+      _result.encrypted = encrypted;
+    }
+    if (file != null) {
+      _result.file = file;
+    }
+    if (link != null) {
+      _result.link = link;
+    }
+    if (call != null) {
+      _result.call = call;
+    }
+    return _result;
+  }
+  factory Meta.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory Meta.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  Meta clone() => Meta()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  Meta copyWith(void Function(Meta) updates) => super.copyWith((message) => updates(message as Meta)) as Meta; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static Meta create() => Meta._();
+  Meta createEmptyInstance() => create();
+  static $pb.PbList<Meta> createRepeated() => $pb.PbList<Meta>();
+  @$core.pragma('dart2js:noInline')
+  static Meta getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Meta>(create);
+  static Meta? _defaultInstance;
+
+  Meta_Type whichType() => _Meta_TypeByTag[$_whichOneof(0)]!;
+  void clearType() => clearField($_whichOneof(0));
+
+  @$pb.TagNumber(1)
+  $fixnum.Int64 get createdOn => $_getI64(0);
+  @$pb.TagNumber(1)
+  set createdOn($fixnum.Int64 v) { $_setInt64(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasCreatedOn() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearCreatedOn() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $fixnum.Int64 get index => $_getI64(1);
+  @$pb.TagNumber(2)
+  set index($fixnum.Int64 v) { $_setInt64(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasIndex() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearIndex() => clearField(2);
+
+  @$pb.TagNumber(3)
+  $17.Uid get sender => $_getN(2);
+  @$pb.TagNumber(3)
+  set sender($17.Uid v) { setField(3, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasSender() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearSender() => clearField(3);
+  @$pb.TagNumber(3)
+  $17.Uid ensureSender() => $_ensure(2);
+
+  @$pb.TagNumber(4)
+  $fixnum.Int64 get messageId => $_getI64(3);
+  @$pb.TagNumber(4)
+  set messageId($fixnum.Int64 v) { $_setInt64(3, v); }
+  @$pb.TagNumber(4)
+  $core.bool hasMessageId() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearMessageId() => clearField(4);
+
+  @$pb.TagNumber(5)
+  $core.bool get encrypted => $_getBF(4);
+  @$pb.TagNumber(5)
+  set encrypted($core.bool v) { $_setBool(4, v); }
+  @$pb.TagNumber(5)
+  $core.bool hasEncrypted() => $_has(4);
+  @$pb.TagNumber(5)
+  void clearEncrypted() => clearField(5);
+
+  @$pb.TagNumber(6)
+  $15.File get file => $_getN(5);
+  @$pb.TagNumber(6)
+  set file($15.File v) { setField(6, v); }
+  @$pb.TagNumber(6)
+  $core.bool hasFile() => $_has(5);
+  @$pb.TagNumber(6)
+  void clearFile() => clearField(6);
+  @$pb.TagNumber(6)
+  $15.File ensureFile() => $_ensure(5);
+
+  @$pb.TagNumber(7)
+  Link get link => $_getN(6);
+  @$pb.TagNumber(7)
+  set link(Link v) { setField(7, v); }
+  @$pb.TagNumber(7)
+  $core.bool hasLink() => $_has(6);
+  @$pb.TagNumber(7)
+  void clearLink() => clearField(7);
+  @$pb.TagNumber(7)
+  Link ensureLink() => $_ensure(6);
+
+  @$pb.TagNumber(8)
+  $26.CallInfo get call => $_getN(7);
+  @$pb.TagNumber(8)
+  set call($26.CallInfo v) { setField(8, v); }
+  @$pb.TagNumber(8)
+  $core.bool hasCall() => $_has(7);
+  @$pb.TagNumber(8)
+  void clearCall() => clearField(8);
+  @$pb.TagNumber(8)
+  $26.CallInfo ensureCall() => $_ensure(7);
+}
+
+class Link extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Link', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.models'), createEmptyInstance: create)
+    ..pPS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'urls')
+    ..hasRequiredFields = false
+  ;
+
+  Link._() : super();
+  factory Link({
+    $core.Iterable<$core.String>? urls,
+  }) {
+    final _result = create();
+    if (urls != null) {
+      _result.urls.addAll(urls);
+    }
+    return _result;
+  }
+  factory Link.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory Link.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  Link clone() => Link()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  Link copyWith(void Function(Link) updates) => super.copyWith((message) => updates(message as Link)) as Link; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static Link create() => Link._();
+  Link createEmptyInstance() => create();
+  static $pb.PbList<Link> createRepeated() => $pb.PbList<Link>();
+  @$core.pragma('dart2js:noInline')
+  static Link getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Link>(create);
+  static Link? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.List<$core.String> get urls => $_getList(0);
+}
+
---
+++ lib/pub/v1/models/meta.pbjson.dart
@@ -0,0 +1,40 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/meta.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use metaDescriptor instead')
+const Meta$json = const {
+  '1': 'Meta',
+  '2': const [
+    const {'1': 'created_on', '3': 1, '4': 1, '5': 3, '10': 'createdOn'},
+    const {'1': 'index', '3': 2, '4': 1, '5': 3, '10': 'index'},
+    const {'1': 'sender', '3': 3, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Uid', '10': 'sender'},
+    const {'1': 'message_id', '3': 4, '4': 1, '5': 3, '10': 'messageId'},
+    const {'1': 'encrypted', '3': 5, '4': 1, '5': 8, '10': 'encrypted'},
+    const {'1': 'file', '3': 6, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '9': 0, '10': 'file'},
+    const {'1': 'link', '3': 7, '4': 1, '5': 11, '6': '.proto.pub.v1.models.Link', '9': 0, '10': 'link'},
+    const {'1': 'call', '3': 8, '4': 1, '5': 11, '6': '.proto.pub.v1.models.CallInfo', '9': 0, '10': 'call'},
+  ],
+  '8': const [
+    const {'1': 'type'},
+  ],
+};
+
+/// Descriptor for `Meta`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List metaDescriptor = $convert.base64Decode('CgRNZXRhEh0KCmNyZWF0ZWRfb24YASABKANSCWNyZWF0ZWRPbhIUCgVpbmRleBgCIAEoA1IFaW5kZXgSMAoGc2VuZGVyGAMgASgLMhgucHJvdG8ucHViLnYxLm1vZGVscy5VaWRSBnNlbmRlchIdCgptZXNzYWdlX2lkGAQgASgDUgltZXNzYWdlSWQSHAoJZW5jcnlwdGVkGAUgASgIUgllbmNyeXB0ZWQSLwoEZmlsZRgGIAEoCzIZLnByb3RvLnB1Yi52MS5tb2RlbHMuRmlsZUgAUgRmaWxlEi8KBGxpbmsYByABKAsyGS5wcm90by5wdWIudjEubW9kZWxzLkxpbmtIAFIEbGluaxIzCgRjYWxsGAggASgLMh0ucHJvdG8ucHViLnYxLm1vZGVscy5DYWxsSW5mb0gAUgRjYWxsQgYKBHR5cGU=');
+@$core.Deprecated('Use linkDescriptor instead')
+const Link$json = const {
+  '1': 'Link',
+  '2': const [
+    const {'1': 'urls', '3': 1, '4': 3, '5': 9, '10': 'urls'},
+  ],
+};
+
+/// Descriptor for `Link`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List linkDescriptor = $convert.base64Decode('CgRMaW5rEhIKBHVybHMYASADKAlSBHVybHM=');
