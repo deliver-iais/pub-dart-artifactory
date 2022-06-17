1ef418b0e085970647db5d899ae2eb7fbe78b0b4
deliver-protocol-publisher <bitbeter@mmmm.cc>
2022-06-17 05:39:25 +0000

    UPDATE to 1.12.13-rc.16

---
+++ lib/pub/v1/avatar.pb.dart
@@ -0,0 +1,96 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/avatar.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+import 'models/uid.pb.dart' as $17;
+import 'models/avatar.pb.dart' as $18;
+
+class GetAvatarReq extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'GetAvatarReq', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..pc<$17.Uid>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'uidList', $pb.PbFieldType.PM, subBuilder: $17.Uid.create)
+    ..hasRequiredFields = false
+  ;
+
+  GetAvatarReq._() : super();
+  factory GetAvatarReq({
+    $core.Iterable<$17.Uid>? uidList,
+  }) {
+    final _result = create();
+    if (uidList != null) {
+      _result.uidList.addAll(uidList);
+    }
+    return _result;
+  }
+  factory GetAvatarReq.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory GetAvatarReq.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  GetAvatarReq clone() => GetAvatarReq()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  GetAvatarReq copyWith(void Function(GetAvatarReq) updates) => super.copyWith((message) => updates(message as GetAvatarReq)) as GetAvatarReq; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarReq create() => GetAvatarReq._();
+  GetAvatarReq createEmptyInstance() => create();
+  static $pb.PbList<GetAvatarReq> createRepeated() => $pb.PbList<GetAvatarReq>();
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarReq getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<GetAvatarReq>(create);
+  static GetAvatarReq? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.List<$17.Uid> get uidList => $_getList(0);
+}
+
+class GetAvatarRes extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'GetAvatarRes', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.avatar'), createEmptyInstance: create)
+    ..pc<$18.Avatar>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'avatar', $pb.PbFieldType.PM, subBuilder: $18.Avatar.create)
+    ..hasRequiredFields = false
+  ;
+
+  GetAvatarRes._() : super();
+  factory GetAvatarRes({
+    $core.Iterable<$18.Avatar>? avatar,
+  }) {
+    final _result = create();
+    if (avatar != null) {
+      _result.avatar.addAll(avatar);
+    }
+    return _result;
+  }
+  factory GetAvatarRes.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory GetAvatarRes.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  GetAvatarRes clone() => GetAvatarRes()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  GetAvatarRes copyWith(void Function(GetAvatarRes) updates) => super.copyWith((message) => updates(message as GetAvatarRes)) as GetAvatarRes; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarRes create() => GetAvatarRes._();
+  GetAvatarRes createEmptyInstance() => create();
+  static $pb.PbList<GetAvatarRes> createRepeated() => $pb.PbList<GetAvatarRes>();
+  @$core.pragma('dart2js:noInline')
+  static GetAvatarRes getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<GetAvatarRes>(create);
+  static GetAvatarRes? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.List<$18.Avatar> get avatar => $_getList(0);
+}
+
