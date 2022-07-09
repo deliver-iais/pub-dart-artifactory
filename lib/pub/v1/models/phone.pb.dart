8b2ab0fb7a91e489005b254717b772ac34993112
deliver-protocol-publisher <bitbeter@mmmm.cc>
2022-07-09 04:18:46 +0000

    UPDATE to 1.12.13-rc.22

---
+++ lib/pub/v1/models/phone.pb.dart
@@ -0,0 +1,73 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/phone.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+import 'dart:core' as $core;
+
+import 'package:fixnum/fixnum.dart' as $fixnum;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class PhoneNumber extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'PhoneNumber', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'proto.pub.v1.models'), createEmptyInstance: create)
+    ..a<$core.int>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'countryCode', $pb.PbFieldType.O3)
+    ..aInt64(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'nationalNumber')
+    ..hasRequiredFields = false
+  ;
+
+  PhoneNumber._() : super();
+  factory PhoneNumber({
+    $core.int? countryCode,
+    $fixnum.Int64? nationalNumber,
+  }) {
+    final _result = create();
+    if (countryCode != null) {
+      _result.countryCode = countryCode;
+    }
+    if (nationalNumber != null) {
+      _result.nationalNumber = nationalNumber;
+    }
+    return _result;
+  }
+  factory PhoneNumber.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory PhoneNumber.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  PhoneNumber clone() => PhoneNumber()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  PhoneNumber copyWith(void Function(PhoneNumber) updates) => super.copyWith((message) => updates(message as PhoneNumber)) as PhoneNumber; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static PhoneNumber create() => PhoneNumber._();
+  PhoneNumber createEmptyInstance() => create();
+  static $pb.PbList<PhoneNumber> createRepeated() => $pb.PbList<PhoneNumber>();
+  @$core.pragma('dart2js:noInline')
+  static PhoneNumber getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<PhoneNumber>(create);
+  static PhoneNumber? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.int get countryCode => $_getIZ(0);
+  @$pb.TagNumber(1)
+  set countryCode($core.int v) { $_setSignedInt32(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasCountryCode() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearCountryCode() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $fixnum.Int64 get nationalNumber => $_getI64(1);
+  @$pb.TagNumber(2)
+  set nationalNumber($fixnum.Int64 v) { $_setInt64(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasNationalNumber() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearNationalNumber() => clearField(2);
+}
+
---
+++ lib/pub/v1/models/sticker.pbjson.dart
@@ -0,0 +1,37 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/sticker.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use stickerDescriptor instead')
+const Sticker$json = const {
+  '1': 'Sticker',
+  '2': const [
+    const {'1': 'file', '3': 1, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '10': 'file'},
+    const {'1': 'emojis', '3': 2, '4': 3, '5': 9, '10': 'emojis'},
+    const {'1': 'is_animated', '3': 3, '4': 1, '5': 8, '10': 'isAnimated'},
+    const {'1': 'is_video', '3': 4, '4': 1, '5': 8, '10': 'isVideo'},
+    const {'1': 'sticker_set_id', '3': 5, '4': 1, '5': 9, '10': 'stickerSetId'},
+  ],
+};
+
+/// Descriptor for `Sticker`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List stickerDescriptor = $convert.base64Decode('CgdTdGlja2VyEi0KBGZpbGUYASABKAsyGS5wcm90by5wdWIudjEubW9kZWxzLkZpbGVSBGZpbGUSFgoGZW1vamlzGAIgAygJUgZlbW9qaXMSHwoLaXNfYW5pbWF0ZWQYAyABKAhSCmlzQW5pbWF0ZWQSGQoIaXNfdmlkZW8YBCABKAhSB2lzVmlkZW8SJAoOc3RpY2tlcl9zZXRfaWQYBSABKAlSDHN0aWNrZXJTZXRJZA==');
+@$core.Deprecated('Use stickerSetDescriptor instead')
+const StickerSet$json = const {
+  '1': 'StickerSet',
+  '2': const [
+    const {'1': 'id', '3': 1, '4': 1, '5': 9, '10': 'id'},
+    const {'1': 'name', '3': 2, '4': 1, '5': 9, '10': 'name'},
+    const {'1': 'thumbnail', '3': 4, '4': 1, '5': 11, '6': '.proto.pub.v1.models.File', '10': 'thumbnail'},
+    const {'1': 'stickers', '3': 3, '4': 3, '5': 11, '6': '.proto.pub.v1.models.Sticker', '10': 'stickers'},
+  ],
+};
+
+/// Descriptor for `StickerSet`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List stickerSetDescriptor = $convert.base64Decode('CgpTdGlja2VyU2V0Eg4KAmlkGAEgASgJUgJpZBISCgRuYW1lGAIgASgJUgRuYW1lEjcKCXRodW1ibmFpbBgEIAEoCzIZLnByb3RvLnB1Yi52MS5tb2RlbHMuRmlsZVIJdGh1bWJuYWlsEjgKCHN0aWNrZXJzGAMgAygLMhwucHJvdG8ucHViLnYxLm1vZGVscy5TdGlja2VyUghzdGlja2Vycw==');
